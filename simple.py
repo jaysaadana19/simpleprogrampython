81cf1e6bfed11a2639a6303ecad5dd3e6dc76e38
Jay Saadana <145225731+jaysaadana19@users.noreply.github.com>
2023-09-24 12:13:08 +0530

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,7 @@
+# Simple Python program to greet the user
+
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Greet the user
+print("Hello, " + name + "! Welcome to this Python program.")
